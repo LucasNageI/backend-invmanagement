e14f830eeaecbfa8ecbe298dee94685efb77af70
LucasNagel <lucasanagel@gmail.com>
2024-12-09 21:49:03 -0300

    bugfix cors

--- src/middlewares/cors.middleware.js
+++ src/middlewares/cors.middleware.js
@@ -1,19 +1,17 @@
 const customCorsMiddleware = (req, res, next) => {
     const origin = req.headers.origin
     const allowedOrigins = [
-      "https://frontend-invmanagement.vercel.app",
+      ENVIRONMENT.FRONTEND_URL,
       "http://localhost:5173",
     ]
     if (allowedOrigins.includes(origin)) {
       res.setHeader("Access-Control-Allow-Origin", origin)
-      res.setHeader("Access-Control-Allow-Credentials", "true") 
     }
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept")
-
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    res.setHeader("Access-Control-Allow-Credentials", "true")
     if (req.method === "OPTIONS") {
-      return res.sendStatus(204)
+      return res.status(200).end()
     }
     next()
   }
-  export default customCorsMiddleware
